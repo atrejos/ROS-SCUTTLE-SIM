1178319941d2611cf8e67081d2fb3e7688dd0e56
atrejos <alextre.edu@gmail.com>
2020-05-04 02:30:33 +0000

    first commit

---
+++ build/scuttle_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/simulation_ws/src/scuttle_description
+# Build directory: /home/user/simulation_ws/build/scuttle_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
